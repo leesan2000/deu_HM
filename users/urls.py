e8c1809612dc5252f4f71df361d06f67de1f43cd
Mariano Rodriguez <rodriguezmesamariano@gmail.com>
2023-07-19 14:00:54 -0300

    Login y registro 'finalizados'

--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('signup/', views.register_user, name='signup'),  # Vista de registro de usuarios
-    # Otras rutas para las vistas de inicio de sesión, cierre de sesión, etc., si es necesario
+    path('login/', views.user_login, name='login'),  # Vista de inicio de sesion de usuarios
 ]
