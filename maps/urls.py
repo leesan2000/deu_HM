c9585497062690fcbd9565da0d4d1743d07c97ab
leesan2000 <93133502+leesan2000@users.noreply.github.com>
2023-07-21 14:19:07 -0300

    formulario notas y vista fuera navbar en base.html

--- maps/urls.py
+++ maps/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 from .views import AddressView
+from .views import AddNoteView
 
 
 urlpatterns = [
     path('home', AddressView.as_view(), name='home'),
+    path('add', AddNoteView.as_view(), name='add'),
 ]
--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.views.generic.edit import CreateView
 from .models import Address
+from .models import Note
+from .forms import NoteForm
 
 
 class AddressView(CreateView):
@@ -15,3 +17,9 @@ class AddressView(CreateView):
         context['mapbox_access_token'] = 'pk.eyJ1IjoibGVlc2FuNjQiLCJhIjoiY2xrNzduejE0MDV0dDNnbjR0cDVtNnc4ciJ9.nA8U773QrxdRkRZiw8TlnA'
         context['addresses'] = Address.objects.all()
         return context
+    
+class AddNoteView(CreateView):
+    model = Note
+    form_class = NoteForm
+    template_name = 'add_note.html'
+    #fields = '__all__'
