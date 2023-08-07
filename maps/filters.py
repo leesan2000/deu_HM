10ddb636d42c066f7c8b148b02926c4d4e2fd882
leesan2000 <93133502+leesan2000@users.noreply.github.com>
2023-08-07 11:50:18 -0300

    boton detalle notas + filtros ocultos

--- maps/filters.py
+++ maps/filters.py
@@ -1,5 +1,7 @@
 import django_filters
 from .models import Note
+from django import forms
+
 
 class notefilter(django_filters.FilterSet):
 
@@ -8,6 +10,14 @@ class notefilter(django_filters.FilterSet):
     class Meta:
         model = Note
         fields = ['autor','entrevista', 'entrevistado', 'campo_asociado']
+
+        widgets = {
+            
+            'autor': forms.TextInput(attrs={'class': 'form-control'}),
+            'entrevista' : forms.TextInput(attrs={'class': 'form-control'}),
+            'entrevistado' : forms.Select(attrs={'class':'form-control'}),
+            'campo_asociado' : forms.TextInput(attrs={'class':'form-control'})
+        }
 
     def __init__(self, *args, **kwargs):
        super(notefilter, self).__init__(*args, **kwargs)
