b272fe30bca2aace6aa3e8e66fd19f67727b9170
Lisandro Vicens <93133502+leesan2000@users.noreply.github.com>
2024-02-23 13:00:55 -0300

    psyucopg2 change

--- deu_HM/settings.py
+++ deu_HM/settings.py
@@ -98,7 +98,7 @@ WSGI_APPLICATION = 'deu_HM.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'deuDATABASE',
         'USER': 'leesan',
         'PASSWORD': '1234',
