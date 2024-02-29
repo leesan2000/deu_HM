4b2b29c497c891ece398fd8b473748751d12103f
Lisandro Vicens <93133502+leesan2000@users.noreply.github.com>
2024-02-29 16:10:20 -0300

    a ver

--- deu_HM/settings.py
+++ deu_HM/settings.py
@@ -170,7 +170,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'deu_HM/static'),)
