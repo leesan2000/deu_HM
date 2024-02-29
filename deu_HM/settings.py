222713a2d35bbdb7b56839dd6e4c64e68226aa83
Lisandro Vicens <93133502+leesan2000@users.noreply.github.com>
2024-02-29 14:45:14 -0300

    staticfiles

--- deu_HM/settings.py
+++ deu_HM/settings.py
@@ -166,6 +166,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+
+STATICFILES_FINDERS = (
+    'django.contrib.staticfiles.finders.FileSystemFinder',
+    #'django.contrib.staticfiles.finders.AppDirectoriesFinder',    #causes verbose duplicate notifications in django 1.9
+)
+
 STATIC_URL = '/static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
